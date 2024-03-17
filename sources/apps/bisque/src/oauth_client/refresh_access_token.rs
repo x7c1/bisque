6660ab31632d6cbac067d6197c2901a654775ed5
x7c1 <jmtaro+x7c1@gmail.com>
2024-03-18 01:36:39 +0900

    impl refresh_access_token

---
+++ sources/apps/bisque/src/oauth_client/refresh_access_token.rs
@@ -0,0 +1,30 @@
+use crate::oauth_client::{AccessToken, OAuthClient, RefreshToken};
+
+#[derive(Debug, serde::Deserialize)]
+pub struct RefreshAccessTokenResponse {
+    pub access_token: AccessToken,
+    pub expires_in: u32,
+    pub scope: String,
+}
+
+impl OAuthClient {
+    /// https://developers.google.com/identity/protocols/oauth2/web-server#offline
+    pub fn refresh_access_token(&self, refresh_token: &RefreshToken) -> crate::Result<AccessToken> {
+        let client = reqwest::blocking::Client::new();
+        let params = [
+            ("client_id", self.client_id.as_str()),
+            ("client_secret", self.client_secret.as_str()),
+            ("refresh_token", refresh_token.as_str()),
+            ("grant_type", "refresh_token"),
+        ];
+        let response = client.post(Self::TOKEN_URL).form(&params).send()?;
+        println!(
+            "[refresh_access_token] Response status: {}",
+            response.status()
+        );
+        let response = response.json::<RefreshAccessTokenResponse>()?;
+        println!("[refresh_access_token] Response body: {:#?}", response);
+
+        Ok(response.access_token)
+    }
+}
