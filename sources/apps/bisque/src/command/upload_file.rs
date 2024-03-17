7d27f9fc7847f41db71dffaa5810d3e031e05b6a
x7c1 <jmtaro+x7c1@gmail.com>
2024-03-18 01:55:36 +0900

    add mod: command::upload_file

---
+++ sources/apps/bisque/src/command/upload_file.rs
@@ -0,0 +1,29 @@
+use crate::access_token_loader::AccessTokenLoader;
+use crate::google_drive_client::{GoogleDriveClient, UploadFileParams};
+use crate::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(long)]
+    file_path: String,
+    #[clap(long)]
+    folder_id: String,
+}
+
+pub fn run(args: Args) -> Result<()> {
+    let access_token_loader = AccessTokenLoader::setup()?;
+    let access_token = access_token_loader.load()?;
+
+    let drive_client = GoogleDriveClient::new(access_token);
+    let file_path = args.file_path;
+    drive_client.upload_file(UploadFileParams {
+        src_file_path: file_path.to_string(),
+        // TODO: remove unwrap
+        dst_name: file_path.split("/").last().unwrap().to_string(),
+        dst_folder_id: args.folder_id,
+    })?;
+
+    Ok(())
+}
