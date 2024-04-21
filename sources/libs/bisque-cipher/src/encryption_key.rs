ea70e44206c1fba7fa7e29e03b8a95eed76c5593
x7c1 <jmtaro+x7c1@gmail.com>
2024-04-21 20:48:32 +0900

    use const

--- sources/libs/bisque-cipher/src/encryption_key.rs
+++ sources/libs/bisque-cipher/src/encryption_key.rs
@@ -5,28 +5,36 @@ use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use std::fs;
 
-pub struct EncryptionKey([u8; 32]);
+pub struct EncryptionKey([u8; Self::SIZE]);
 
 impl EncryptionKey {
-    pub fn generate() -> EncryptionKey {
-        let mut seed: [u8; 64] = [0; 64];
-        let mut rng = StdRng::from_entropy();
-        rng.fill(&mut seed);
+    const SIZE: usize = 32;
 
-        let mut key = [0; 32];
-        key.copy_from_slice(&seed[..32]);
+    pub fn generate() -> EncryptionKey {
+        let seed = Self::generate_seed();
+        let mut key = [0; Self::SIZE];
+        key.copy_from_slice(&seed[..Self::SIZE]);
         EncryptionKey(key)
     }
+
     pub fn from_file(path: String) -> Result<Self> {
         let bytes = fs::read(path).unwrap();
-        if bytes.len() != 32 {
+        if bytes.len() != Self::SIZE {
             todo!()
         }
-        let mut array = [0; 32];
+        let mut array = [0; Self::SIZE];
         array.copy_from_slice(&bytes);
         Ok(EncryptionKey(array))
     }
+
     pub fn as_array(&self) -> &GenericArray<u8, U32> {
         GenericArray::from_slice(&self.0)
+    }
+
+    fn generate_seed() -> [u8; Self::SIZE * 2] {
+        let mut seed = [0; Self::SIZE * 2];
+        let mut rng = StdRng::from_entropy();
+        rng.fill(&mut seed);
+        seed
     }
 }
