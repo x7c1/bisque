03a870418e835520601fc724ad0772a9980bd8de
x7c1 <jmtaro+x7c1@gmail.com>
2024-04-21 19:53:08 +0900

    modify timeout duration

--- sources/apps/bisque/src/google_drive_client/mod.rs
+++ sources/apps/bisque/src/google_drive_client/mod.rs
@@ -1,10 +1,12 @@
-use crate::oauth_client::AccessToken;
-
 mod upload_file;
 pub use upload_file::UploadFileParams;
 
 mod multipart;
 pub use multipart::Metadata;
+
+use crate::oauth_client::AccessToken;
+use crate::{here, Result};
+use std::time::Duration;
 
 pub struct GoogleDriveClient {
     client: reqwest::blocking::Client,
@@ -12,10 +14,15 @@ pub struct GoogleDriveClient {
 }
 
 impl GoogleDriveClient {
-    pub fn new(access_token: AccessToken) -> Self {
-        Self {
-            client: reqwest::blocking::Client::new(),
+    pub fn new(access_token: AccessToken) -> Result<Self> {
+        let client = reqwest::blocking::Client::builder()
+            .timeout(Duration::from_secs(60))
+            .build()
+            .map_err(here!())?;
+
+        Ok(Self {
+            client,
             access_token,
-        }
+        })
     }
 }
