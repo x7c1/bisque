df536480fed8634699fbcb674555a51f8561a8d7
x7c1 <jmtaro+x7c1@gmail.com>
2024-04-28 20:24:56 +0900

    modify encryptor

--- sources/libs/bisque-cipher/src/encryptor.rs
+++ sources/libs/bisque-cipher/src/encryptor.rs
@@ -1,43 +1,60 @@
-use crate::EncryptionKey;
-use aes::cipher::generic_array::GenericArray;
-use aes::cipher::{BlockEncrypt, KeyInit};
-use aes::Aes256;
+use crate::Error::CannotCreateEncryptor;
+use crate::Result;
+use openssl::symm::{Cipher, Crypter, Mode};
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
+use std::process::exit;
 
 const BLOCK_SIZE: usize = 16;
 
 pub struct Encryptor<R> {
     inner: R,
-    cipher: Aes256,
+    crypter: Crypter,
+    block_size: usize,
+    finalized: bool,
 }
 
 impl<R: Read> Encryptor<R> {
-    pub fn new(reader: R, key: &EncryptionKey) -> Self {
-        Encryptor {
+    pub fn new(reader: R, key: &[u8], iv: &[u8]) -> Result<Self> {
+        let cipher = Cipher::aes_256_cbc();
+        let crypter = Crypter::new(cipher, Mode::Encrypt, key, Some(iv))
+            .map_err(|cause| CannotCreateEncryptor { cause })?;
+
+        Ok(Encryptor {
             inner: reader,
-            cipher: Aes256::new(key.as_array()),
+            crypter,
+            block_size: cipher.block_size(),
+            finalized: false,
+        })
+    }
+
+    fn finalize(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
+        if self.finalized {
+            return Ok(0);
         }
+        let mut output = vec![0; self.block_size];
+        let written = self.crypter.finalize(&mut output)?;
+        buf.write_all(&output)?;
+        self.finalized = true;
+        Ok(written)
     }
 }
 
 impl<R: Read> Read for Encryptor<R> {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let mut inner_buffer = [0; BLOCK_SIZE];
-        let bytes_read = self.inner.read(&mut inner_buffer)?;
-        if bytes_read == 0 {
-            return Ok(0); // End of inner stream
-        }
-        let mut chunk = inner_buffer.to_vec();
-        let mut position = 0;
-        while position < chunk.len() {
-            let block = GenericArray::from_mut_slice(&mut chunk[position..position + BLOCK_SIZE]);
-            self.cipher.encrypt_block(block);
-            position += BLOCK_SIZE;
-        }
-        let to_copy = std::cmp::min(buf.len(), chunk.len());
-        buf[..to_copy].copy_from_slice(&chunk[..to_copy]);
+    fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
+        let mut cache = vec![0; buf.len()];
+        let loaded = self.inner.read(&mut cache)?;
 
-        Ok(to_copy)
+        let mut output = vec![0; loaded + self.block_size];
+        let written = {
+            let input = &cache[..loaded];
+            self.crypter.update(input, &mut output)?
+        };
+        if written == 0 {
+            self.finalize(&mut buf)
+        } else {
+            buf.write_all(&output[..written])?;
+            Ok(written)
+        }
     }
 }
